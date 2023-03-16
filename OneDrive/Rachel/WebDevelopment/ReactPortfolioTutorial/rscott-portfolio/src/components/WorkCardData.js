c3355d6dc55e83f67a9ce29338b2d3bed79ba5fa
r-scott25 <rachelbernal2@gmail.com>
2023-03-16 11:11:37 -0500

    added Little Lemon reservation

---
+++ OneDrive/Rachel/WebDevelopment/ReactPortfolioTutorial/rscott-portfolio/src/components/WorkCardData.js
@@ -0,0 +1,52 @@
+import AO2 from "../assets/AO2.png";
+import LuckyShrubHome from "../assets/LuckyShrubHome.png";
+import LuckyShrub from "../assets/LuckyShrub.png";
+import PetesPortfolio from "../assets/PetesPortfolio.png";
+
+
+const ProjectCardData = [
+
+    {
+       imgsrc:LittleLemonReservation,
+       title: "Little Lemon Reservation",
+       text: "In this project for Meta's UX/UI course, I completed the UX/UI design process to create an interactive prototype of a reservation feature for Little Lemon Restaurant.",
+       view: "https://www.figma.com/proto/JEAthChh8VvmMYwDK2t7PU/Little-Lemon-Table-Reservation?page-id=22%3A1924&node-id=22-2521&viewport=1251%2C1155%2C0.55&scaling=scale-down&starting-point-node-id=22%3A2521",
+       source: "https://www.figma.com/file/JEAthChh8VvmMYwDK2t7PU/Little-Lemon-Table-Reservation?node-id=0%3A1&t=r296CxyP5PsQuumr-1",
+    },
+
+  {
+    imgsrc: PetesPortfolio,
+    title: "Pete's Portfolio",
+    text: "Pete's Portfolio was created as a capstone project in the Meta Advanced React course.",
+    view: "https://meta-react-portfolio-project.vercel.app/#contact-me",
+    source: "https://github.com/r-scott25/meta-react-portfolio",
+ },
+  {
+    imgsrc: LuckyShrub,
+    title: "Lucky Shrub",
+    text:
+      "The Lucky Shrub homepage is a project I completed as a practice in responsive HTML and CSS.",
+    view: "https://lucky-shrub-3sjs9t05h-r-scott25.vercel.app/",
+    source: "https://github.com/r-scott25/LuckyShrub",
+  },
+  {
+    imgsrc: AO2,
+    title: "Adventure Outdoors",
+    text:
+      "Adventure Outdoors is an outdoor gear rental website that my group and I created as a capstone project while enrolled in LaunchCode's Liftoff program.",
+    view: "https://github.com/r-scott25/AdventureOutdoors.git",
+    source: "https://github.com/r-scott25/AdventureOutdoors.git",
+  },
+
+  
+
+  //   {
+  //      imgsrc: pro3,
+  //      title: "Project 3 Title",
+  //      text: "This is my third project.",
+  //      view: "url.com",
+  //      source: "url.com",
+  //   },
+];
+
+export default ProjectCardData;
